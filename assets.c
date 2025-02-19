067dd036662a347b01087da0b33d9af8052ad62d
Gohy Leandre <leandre.gohy@hexeo.be>
2025-02-19 17:32:39 +0100

    add characters assets

--- assets.c
+++ assets.c
@@ -13,6 +13,34 @@ extern uint8_t gAssetBuffer[0x2800];
 extern zrom_t zroms[3];
 static void *buffer_ptr = gAssetBuffer;
 
+uint8_t *zasset_ki1_characters[] = {
+    zasset_ki1_cinder,
+    zasset_ki1_combo,
+    zasset_ki1_eyedol,
+    zasset_ki1_fulgore,
+    zasset_ki1_glacius,
+    zasset_ki1_jago,
+    zasset_ki1_orchid,
+    zasset_ki1_riptor,
+    zasset_ki1_sabrewolf,
+    zasset_ki1_spinal,
+    zasset_ki1_thunder,
+};
+
+uint8_t *zasset_ki2_characters[] = {
+    zasset_ki2_combo,
+    zasset_ki2_fulgore,
+    zasset_ki2_gargos,
+    zasset_ki2_glacius,
+    zasset_ki2_jago,
+    zasset_ki2_kim_wu,
+    zasset_ki2_maya,
+    zasset_ki2_orchid,
+    zasset_ki2_sabrewulf,
+    zasset_ki2_spinal,
+    zasset_ki2_tusk,
+};
+
 void *zasset_load(const void *ptr)
 {
     void *out = buffer_ptr;
--- assets.h
+++ assets.h
@@ -16,6 +16,31 @@ typedef struct
 } zrom_t;
 
 extern uint8_t zasset_logo[];
+extern uint8_t zasset_ki1_cinder[];
+extern uint8_t zasset_ki1_combo[];
+extern uint8_t zasset_ki1_eyedol[];
+extern uint8_t zasset_ki1_fulgore[];
+extern uint8_t zasset_ki1_glacius[];
+extern uint8_t zasset_ki1_jago[];
+extern uint8_t zasset_ki1_orchid[];
+extern uint8_t zasset_ki1_riptor[];
+extern uint8_t zasset_ki1_sabrewolf[];
+extern uint8_t zasset_ki1_spinal[];
+extern uint8_t zasset_ki1_thunder[];
+extern uint8_t zasset_ki2_combo[];
+extern uint8_t zasset_ki2_fulgore[];
+extern uint8_t zasset_ki2_gargos[];
+extern uint8_t zasset_ki2_glacius[];
+extern uint8_t zasset_ki2_jago[];
+extern uint8_t zasset_ki2_kim_wu[];
+extern uint8_t zasset_ki2_maya[];
+extern uint8_t zasset_ki2_orchid[];
+extern uint8_t zasset_ki2_sabrewulf[];
+extern uint8_t zasset_ki2_spinal[];
+extern uint8_t zasset_ki2_tusk[];
+
+extern uint8_t *zasset_ki1_characters[];
+extern uint8_t *zasset_ki2_characters[];
 
 void *zasset_load(const void *ptr);
 void zasset_clear(void);
